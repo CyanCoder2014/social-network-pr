cfd297620aa7362cc9f08c3347c753ab5e98d42f
CyanCoder2014 <cyancoder2016@gmail.com>
2019-05-12 20:19:03 +0430

    farid changed

--- app/Signup.php
+++ app/Signup.php
@@ -12,8 +12,11 @@ class Signup extends Model
       'science' => 'array',
     ];
     protected $statuses = [
-      0 => 'تایید نشده',
-      1 => 'تایید شده توسط',
+      0 => 'نامشخص',
+      1 => 'تایید عضویت برای بخش پژوهشی',
+      2 => 'تایید عضویت برای بخش آموزش',
+      3 => 'تایید عضویت برای بخش اداری',
+      4 => 'تایید عضویت برای بخش مالی',
     ];
 
     public function transaction(){
@@ -44,7 +47,7 @@ class Signup extends Model
     public function active(){
         if ($this->active== 0)
             return 'اینترنتی';
-        return 'دستی';
+        return 'تایید دستی پرداخت';
     }
 
 }
